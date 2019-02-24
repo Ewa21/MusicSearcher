871b924eab0f2cedc62466a271e644f166d50395
ewa <virtuosity2106@gmail.com>
2019-02-24 23:34:21 +0100

    Added a progress bar to report the working status of the app.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TableView;
 import sample.model.Album;
 import sample.model.Artist;
@@ -16,12 +17,21 @@ public class Controller {
     @FXML
     private TableView artistTable;
 
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
     public void listArtists(){
         Task<ObservableList<Artist>> task = new GetAllArtistsTask();
         artistTable.itemsProperty().bind(task.valueProperty());
+
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressBar.setVisible(true);
+        task.setOnSucceeded(e->progressBar.setVisible(false));
+        task.setOnFailed(e->progressBar.setVisible(false));
         new Thread(task).start();
     }
 
+    @FXML
     public void listAlbumsForArtist(){
         final Artist artist = (Artist) artistTable.getSelectionModel().getSelectedItem();
 
